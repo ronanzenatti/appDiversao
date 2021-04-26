5034214828da5d2eb8b28801c8a006c8bc3c5c8b
ronanzenatti <ronanzenatti@gmail.com>
2021-04-26 09:02:53 -0300

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GodzillaKongPage } from '../filmes/godzilla-kong';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  godzillaKong = GodzillaKongPage;
 
   constructor(public navCtrl: NavController) {
 
