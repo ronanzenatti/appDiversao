3a40a8a57d67ead07c9c6e52c4ebbc6ba5a7317e
ronanzenatti <ronanzenatti@gmail.com>
2021-04-26 09:48:07 -0300

    2 files added

---
+++ pages/filmes/liga-justica/liga-justica.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-liga-justica',
+  templateUrl: 'liga-justica.html'
+})
+export class LigaJusticaPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
