6845adea8a88d1129114c846de17ab6310bc8a07
ronanzenatti <ronanzenatti@gmail.com>
2021-05-14 08:03:42 -0300

    2 files modified

--- pages/filmes/filmes.ts
+++ pages/filmes/filmes.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'filmes.html'
 })
 export class FilmesPage {
+  filmes = [
+    {
+      nome: 'Eurotrip: Passaporte para a Confusão',
+      cartaz:
+        'https://www.themoviedb.org/t/p/w94_and_h141_bestv2/rnzfFPhJZLdJS40nzvhNyfZ0ahD.jpg',
+      lancamento: '20 de fevereiro de 2004'
+    },
+    {
+      nome: 'Minha Mãe é Uma Peça: O Filme',
+      cartaz:
+        'https://www.themoviedb.org/t/p/w94_and_h141_bestv2/5cF5v8N0wMALMcgEHpBZHyYQZDl.jpg',
+      lancamento: '21 de junho de 2013'
+    }
+  ];
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController) {}
 }
