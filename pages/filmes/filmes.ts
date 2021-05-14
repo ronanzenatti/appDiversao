1bba68b872253b051ebc12441346e259b139cf0d
ronanzenatti <ronanzenatti@gmail.com>
2021-05-14 07:37:40 -0300

    2 files added, 2 deleted

--- pages/filmes/filmes.ts
+++ pages/filmes/filmes.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-filmes',
+  templateUrl: 'filmes.html'
 })
-export class AboutPage {
+export class FilmesPage {
 
   constructor(public navCtrl: NavController) {
 
